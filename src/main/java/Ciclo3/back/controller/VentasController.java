4cc64ba521add7e65821f0dd9a4964be66f9e948
Oscar Cita <smalqs@gmail.com>
2021-10-14 16:27:09 -0500

    Funcion crear venta y consultar cliente

--- src/main/java/Ciclo3/back/controller/VentasController.java
+++ src/main/java/Ciclo3/back/controller/VentasController.java
@@ -1,6 +1,27 @@
 package Ciclo3.back.controller;
 
-public class VentasController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import Ciclo3.back.model.Clientes;
+import Ciclo3.back.model.Ventas;
+import Ciclo3.back.model.VentasDAO;
+
+
+@RestController
+public class VentasController {
+	@PostMapping("/venta/crearVenta")
+	public Ventas crearVenta(@RequestBody Ventas ven) {
+		VentasDAO ventadao = new VentasDAO();
+		return ventadao.ingresarVenta(ven);
+	}
+	@PostMapping("/venta/consultarCliente")
+	public Clientes consultarCliente(@RequestBody Clientes cli) {
+		VentasDAO clidao = new VentasDAO();
+		return clidao.consultarClienteVentas(cli);
+	}
 	
+	
+
 }
--- src/main/java/Ciclo3/back/model/VentasDAO.java
+++ src/main/java/Ciclo3/back/model/VentasDAO.java
@@ -1,5 +1,106 @@
 package Ciclo3.back.model;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class VentasDAO {
+
+	Conexion con = new Conexion();
+
+	public Ventas validar(Ventas ven) {
+		Ventas ventaEnc = null;
+		PreparedStatement ps = null;
+		Ventas ventaRet = null;
+		ResultSet rs = null;
+//configurar segun sea necesario el sql
+		String sql = "SELECT * FROM ventas us WHERE codigo_venta like ?";
+		try {
+			ps = con.getConnection().prepareStatement(sql);
+			ps.setLong(1, ven.getCodigo());
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Long codigo = rs.getLong("codigo_venta");
+				Long cedulaUsuario = rs.getLong("usuarios_cedula_usuario");
+				Long cedulaCliente = rs.getLong("clientes_cedula_cliente");
+				Long iva = rs.getLong("ivaventa");
+				Long totalVenta = rs.getLong("total_venta");
+				Long totalValor = rs.getLong("valor_venta");
+				ventaEnc = new Ventas(codigo, cedulaUsuario, cedulaCliente, iva, totalVenta, totalValor);
+			}
+			if (ventaEnc != null) {
+				if (ventaEnc.getCodigo().equals(ven.getCodigo())) {
+					ventaRet = ventaEnc;
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			con.close();
+			try {
+				ps.close();
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return ventaRet;
+	}
+	public Ventas ingresarVenta(Ventas venta) {
+		PreparedStatement ps = null;
+		String sql = "INSERT INTO ventas (codigo_venta, usuarios_cedula_usuario, clientes_cedula_cliente, ivaventa, total_venta, valor_venta) VALUES(?,?,?,?,?,?)";
+		try {
+			ps = con.getConnection().prepareStatement(sql);
+			ps.setLong(1, venta.getCodigo());
+			ps.setLong(2, venta.getCedula_usuario());
+			ps.setLong(3, venta.getCedula_cliente());
+			ps.setFloat(4, venta.getIva());
+			ps.setFloat(5, venta.getTotal_venta());
+			ps.setFloat(6, venta.getValor_venta());
+			ps.execute();
+
+		} catch (SQLException e) {
+			System.err.println(e);
+
+		} finally {
+			con.close();
+		}
+		return venta;
+
+	}
+	public Clientes consultarClienteVentas(Clientes cliente) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Clientes clienteEnc = null;
+		String sql = "SELECT * FROM clientes WHERE cedula_cliente=?";
+		try {
+			ps = con.getConnection().prepareStatement(sql);
+			ps.setLong(1, cliente.getCedula());
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Long cedula = rs.getLong("cedula_cliente");
+				String direccion = rs.getString("direccion_cliente");
+				String correo = rs.getString("email_cliente");
+				String nombre = rs.getString("nombre_cliente");
+				String telefono = rs.getString("telefono_cliente");
+				clienteEnc = new Clientes(cedula, direccion, correo, nombre, telefono);
+
+			}
+
+		} catch (SQLException e) {
+			System.err.println(e);
+
+		} finally {
+			con.close();
+
+		}
+		return clienteEnc;
+
+	}
 
 }
