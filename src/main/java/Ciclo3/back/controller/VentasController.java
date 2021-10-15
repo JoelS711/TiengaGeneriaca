8089a589643e7a9b506052db499932793fe007e1
Oscar Cita <smalqs@gmail.com>
2021-10-15 00:43:41 -0500

    Se crea funcion Ventas, con la opcion de crear venta

--- src/main/java/Ciclo3/back/controller/VentasController.java
+++ src/main/java/Ciclo3/back/controller/VentasController.java
@@ -1,5 +1,23 @@
 package Ciclo3.back.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import Ciclo3.back.model.Clientes;
+import Ciclo3.back.model.Ventas;
+import Ciclo3.back.model.VentasDAO;
+
+
+@RestController
 public class VentasController {
+	@PostMapping("/venta/crearVenta")
+	public Ventas crearVenta(@RequestBody Ventas ven) {
+		VentasDAO ventadao = new VentasDAO();
+		return ventadao.ingresarVenta(ven);
+	}
+	
+	
+	
 
 }
